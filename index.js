b2206274ef703590b03b19c6bf7c3f7c5513c41a
Daniel Glucksman <daniel@grocerkey.com>
2021-03-23 15:08:45 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
+
+
+const octokit = new Octokit();
+const startCommit = core.getInput('start-commit');
+const endCommit = core.getInput('end-commit');
+const repo = core.getInput('repo');
+
+var commits = await octokit.repos
+  .listCommits({
+    owner: "Grocerkey",
+    repo: repo,
+    sha: startCommit
+  });
+
+
+commits.forEach(item => {
+  
+  console.log(item); 
+  
+  if(item.sha == endCommit)
+    break;
+});
+
