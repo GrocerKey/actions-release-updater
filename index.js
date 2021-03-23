0121e221dd5824d3475ebbcbac34bfbb4bf1f25e
Daniel Glucksman <daniel@grocerkey.com>
2021-03-23 15:23:32 +0200

    fix.

--- index.js
+++ index.js
@@ -3,41 +3,27 @@ const github = require('@actions/github');
 const { Octokit } = require("@octokit/rest");
 
 
-export function anAction() {
+async function run() {
+  const octokit = new Octokit();
+  const startCommit = core.getInput('start-commit');
+  const endCommit = core.getInput('end-commit');
+  const repo = core.getInput('repo');
 
-  return async (dispatch) => {
-    try {
-      const octokit = new Octokit();
-      const startCommit = core.getInput('start-commit');
-      const endCommit = core.getInput('end-commit');
-      const repo = core.getInput('repo');
-
-      var commits = await octokit.repos
-      .listCommits({
-        owner: "Grocerkey",
-        repo: repo,
-        sha: startCommit
-      });
+  var commits = await octokit.repos
+  .listCommits({
+    owner: "Grocerkey",
+    repo: repo,
+    sha: startCommit
+  });
 
 
-      commits.forEach(item => {
+  commits.forEach(item => {
 
-          console.log(item); 
+      console.log(item); 
 
-          if(item.sha == endCommit)
-          break;
-      });
-
-
-      dispatch({
-        type: 'AN_ACTION',
-        ...result.data
-      });
-    } catch (error) {
-      console.error(error_;
-      dispatch({
-        type: 'AN_ACTION' // or, better, 'FAILED_ACTION' or something like that
-      });
-    }
-  };
+      if(item.sha == endCommit)
+      break;
+  });
 }
+
+run();
